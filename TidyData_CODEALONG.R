69121f870623a8d1addd02680407c552f24e78c2
Danielle Quinn <quinn.dea@gmail.com>
2023-01-06 14:20:32 -0400

    Create TidyData_CODEALONG.R

---
+++ TidyData_CODEALONG.R
@@ -0,0 +1,25 @@
+# Importing Data ----
+# Look at the names of the files available in your working directory
+
+# parks.csv is in "comma separated values" format
+
+# Exploring Data ----
+# - Preview ----
+
+# - Structure ----
+
+# - Class ----
+
+# - Values ----
+# Columns can be extracted as vectors using $
+
+# Assessing Data ----
+# - Numeric Variables ----
+
+# - Character Variables ----
+
+# - Simple Tables ----
+
+# Exercise ----
+## Each park should have exactly one row in the data
+## Which technique(s) would you use to confirm that no parks are repeated?
